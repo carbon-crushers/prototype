2fb6ac9596de0a8ee64c961b643533305188b180
carbon-crushers <139949004+carbon-crushers@users.noreply.github.com>
2023-07-19 10:30:34 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import bme280
+import smbus2
+from time import sleep
+
+port = 1
+address = 0x77 # Adafruit BME280 address.
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus,address)
+
+while True:
+    bme280_data = bme280.sample(bus,address)
+    humidity  = bme280_data.humidity
+    pressure  = bme280_data.pressure
+    temperature = bme280_data.temperature
+    print(humidity, pressure, temperature)
+    print("Press any key to stop measurements...")
+    sleep(1)
